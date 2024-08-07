4b8faed822892ec4401cda92632e02d0365492ad
vmarkgt <89555285+vmarkgt@users.noreply.github.com>
2024-08-07 15:09:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function updatePaymentFields() {
+    const paymentMethod = document.getElementById('pago').value;
+    const efectivoField = document.getElementById('efectivoField');
+    const creditoField = document.getElementById('creditoField');
+
+    if (paymentMethod === 'efectivo') {
+        efectivoField.style.display = 'block';
+        creditoField.style.display = 'none';
+    } else if (paymentMethod === 'credito') {
+        efectivoField.style.display = 'none';
+        creditoField.style.display = 'block';
+    } else {
+        efectivoField.style.display = 'none';
+        creditoField.style.display = 'none';
+    }
+}
+
 function downloadPDF() {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF({
@@ -7,25 +24,35 @@ function downloadPDF() {
     });
 
     const container = document.querySelector('.container');
+    const button = document.getElementById('download-btn');
+    const timestamp = document.getElementById('timestamp');
+
+    // Ocultar el botón
+    button.style.display = 'none';
+
+    // Mostrar el timestamp
+    const now = new Date();
+    const formattedTimestamp = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+    timestamp.textContent = `Generado el: ${formattedTimestamp}`;
 
     html2canvas(container).then(canvas => {
         const imgData = canvas.toDataURL('image/png');
-        const imgWidth = 255; // Ancho del contenido en el PDF
-        const pageHeight = 396; // Altura de la página tamaño carta en puntos
+        const imgWidth = doc.internal.pageSize.getWidth() * 0.75; // 75% del ancho de la página
+        const pageHeight = doc.internal.pageSize.getHeight(); // Altura de la página tamaño carta
         const imgHeight = canvas.height * imgWidth / canvas.width;
-        const heightLeft = imgHeight;
-        
-        const marginLeft = (doc.internal.pageSize.width - imgWidth) / 2;
-        let position = 0;
+
+        const marginLeft = (doc.internal.pageSize.width - imgWidth) / 2; // Centrado
+        let position = (pageHeight - imgHeight) / 2; // Centrado vertical
 
         doc.addImage(imgData, 'PNG', marginLeft, position, imgWidth, imgHeight);
         doc.save('Formulario_de_Compra.pdf');
+
+        // Mostrar el botón nuevamente
+        button.style.display = 'block';
     });
 }
 
 window.onload = function() {
-    const form = document.getElementById('purchaseForm');
     const now = new Date();
-    const formattedDate = now.toISOString().split('T')[0];
-    form.elements['fechaDocumento'].value = formattedDate;
-};
+    const formattedTimestamp = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+   
